cc639cddc747b3be8fe3c5c16c4ba71703966340
이도현 <ehtm01@gmail.com>
2026-04-22 10:12:47 +0900

    [S14P31S309-289] chore: JPA Auditing 설정을 JpaConfig로 분리

---
+++ backend/src/main/java/com/ssafy/s309/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.ssafy.s309.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
