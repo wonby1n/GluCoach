da5212c43b7ba8edcf9ad5a9bceab5e2c3e625a5
이도현 <ehtm01@gmail.com>
2026-05-01 18:13:18 +0900

    fix: UserDetailsServiceAutoConfiguration 제외 (자동 생성 비밀번호 로그 제거)

--- backend/src/main/java/com/ssafy/s309/BackendApplication.java
+++ backend/src/main/java/com/ssafy/s309/BackendApplication.java
@@ -2,8 +2,9 @@ package com.ssafy.s309;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class BackendApplication {
 
   public static void main(String[] args) {
