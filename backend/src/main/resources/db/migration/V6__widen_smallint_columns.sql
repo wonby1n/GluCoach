62fc812c4dbc17cde090498757129e4b7ae1e4f9
이도현 <ehtm01@gmail.com>
2026-05-01 17:56:31 +0900

    Revert "fix: SMALLINT 매핑 변경에 따른 호출부 타입 정리 (Short 캐스팅, record 필드 변경)"
    This reverts commit 6bc5e5dd9544362b24bca09ed559d71e05b49567.

--- backend/src/main/resources/db/migration/V6__widen_smallint_columns.sql
+++ backend/src/main/resources/db/migration/V6__widen_smallint_columns.sql
@@ -1,4 +1,4 @@
--- 자바 Integer 매핑 정합을 위해 SMALLINT 컬럼들을 INT로 확장
+git commit -m "feat: V6 마이그레이션 - SMALLINT 컬럼을 INT로 확장 (Short cascading 정리)"-- 자바 Integer 매핑 정합을 위해 SMALLINT 컬럼들을 INT로 확장
 ALTER TABLE users ALTER COLUMN age TYPE INT;
 ALTER TABLE users ALTER COLUMN week_start_day TYPE INT;
 ALTER TABLE ward_guardian ALTER COLUMN priority TYPE INT;
