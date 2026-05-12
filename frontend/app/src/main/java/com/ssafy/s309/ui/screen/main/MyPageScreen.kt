e768f2f3483136d29bc0d7a47087ff09b4961a1c
박미영 <a29279@naver.com>
2026-05-12 17:08:47 +0900

    [S14P31S309-1301] feat: 키보드 숫자/특수문자 모드 추가

--- frontend/app/src/main/java/com/ssafy/s309/ui/screen/main/MyPageScreen.kt
+++ frontend/app/src/main/java/com/ssafy/s309/ui/screen/main/MyPageScreen.kt
@@ -2,6 +2,7 @@ package com.ssafy.s309.ui.screen.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
